8d6b654f05e7b48293f711d5cc047757bd672be6
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2024-12-23 21:28:34 +0530

    started Product Page

---
+++ src/Components/Productpages/Productpage1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Productpage1 = () => {
+  return (
+    <div>
+      <h1>ndfdj</h1>
+    </div>
+  )
+}
+
+export default Productpage1
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./Product.css";
+import Product1 from './Productpages/Productpage1.jsx';
 
 const Products = () => {
+   const nextSectionRef = useRef(null);
+  
+    const scrollToNextSection = () => {
+      if (nextSectionRef.current) {
+        nextSectionRef.current.scrollIntoView({ behavior: "smooth" });
+      }
+    };
   return (
     <div>
-      <h1>Our Products</h1>
-      <p>Explore our range of high-quality pharmaceutical products.</p>
+      <div className="product-container">
+        <div className="product-content">
+          <h1 className="product-title">Our Products</h1>
+          <p className="product-subtitle">
+            "Discover our wide range of high-quality pharmaceutical products
+            designed to promote health and well-being."
+          </p>
+          <button className="know-more-btn" onClick={scrollToNextSection}>
+            View Products 
+          </button>
+        </div>
+      </div>
+      <div ref={nextSectionRef}>
+      <Product1/>
+      </div>
     </div>
   );
 };
